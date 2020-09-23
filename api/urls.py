76e472ad9a8a8036678467c2473d25e99815206d
kaylaramirez <ram.kayla@gmail.com>
2020-09-22 23:50:39 -0500

    registered routers for movieviewset and ratingviewset

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-
+from api.views import MovieViewSet, RatingViewSet
 
 router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
+router.register('rating', RatingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
