8eee4232ef5b2cdc7ed7721e48df2961c89590a5
kaylaramirez <ram.kayla@gmail.com>
2020-09-24 13:42:12 -0500

    added authentication tokens and removed hard coded user.

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.authentication import TokenAuthentication
 
 from api.models import Movie, Rating
 from api.serializers import MovieSerializer, RatingSerializer
@@ -11,14 +12,16 @@ from api.serializers import MovieSerializer, RatingSerializer
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    authentication_classes = (TokenAuthentication, )
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
         if 'stars' in request.data:
             movie = Movie.objects.get(id=pk)
             stars = request.data['stars']
-            # user = request.user
-            user = User.objects.get(id=1)
+            user = request.user
+            print('user', user)
+            # user = User.objects.get(id=1)
 
             try:
                 rating = Rating.objects.get(user=user.id, movie=movie.id)
@@ -39,3 +42,4 @@ class MovieViewSet(viewsets.ModelViewSet):
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    authentication_classes = (TokenAuthentication, )
